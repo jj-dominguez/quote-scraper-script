c737541ce8cc1141c3b817f91831e70f62cab225
Jordan Dominguez <jordan@yellowdogsolutions.com>
2018-04-02 22:45:41 -0700

    Updated UX

--- quote-scraper.py
+++ quote-scraper.py
@@ -1,6 +1,14 @@
 #! python3
 # quotescraper.py - Scrapes quotes from www.goodreads.com into .txt file
-# author - Jordan Dominguez
+# # author - Jordan Dominguez
+#  _____         ____
+# /\___ \       /\  _`\
+# \/__/\ \      \ \ \/\ \
+#    _\ \ \      \ \ \ \ \
+#   /\ \_\ \ __   \ \ \_\ \ __
+#   \ \____//\_\   \ \____//\_\
+#    \/___/ \/_/    \/___/ \/_/
+
 
 from lxml import html
 import requests
@@ -10,9 +18,9 @@ import requests
 def parseQuotes(link):
     print('Downloading page %s...' % (link))
     page = requests.get(link)
-    text_tree = html.fromstring(page.text)
     content_tree = html.fromstring(page.content)
     next_page = content_tree.xpath('//a[@class="next_page"]//@href')
+    text_tree = html.fromstring(page.text)
     quotes = text_tree.xpath('//div[@class="quoteText"]/text()')
     quotes = [x.strip() for x in quotes]
     quotes = [x for x in quotes if len(x) > 30]
@@ -26,7 +34,11 @@ def parseQuotes(link):
 
 
 # picking the topic of quotes to scrape (from goodreads.com)
-topic_list = ["love", "life", "inspirational", "humor", "philosophy", "truth", "god", "wisdom", "inspirational-quotes", "happiness", "romance", "hope", "death", "quotes", "poetry", "faith", "writing", "inspiration", "religion", "knowledge", "success", "education", "relationships", "motivational", "life-lessons", "time", "science", "funny", "books", "spirituality"]
+# could eventually add an API call or a tag scraper function to get the list of topics from the site dynamically
+topic_list = ["love", "life", "inspirational", "humor", "philosophy", "truth", "god", "wisdom",
+"inspirational-quotes", "happiness", "romance", "hope", "death", "quotes", "poetry", "faith",
+"writing", "inspiration", "religion", "knowledge", "success", "education", "relationships",
+"motivational", "life-lessons", "time", "science", "funny", "books", "spirituality"]
 
 
 print("List of topics:")
@@ -41,11 +53,11 @@ else:
     quote_page = ('https://www.goodreads.com/quotes/tag/' + topic)
 
 # running the parseQuotes function on the quote page URL created from user topic input
-quotes=parseQuotes(quote_page)
+quotes = parseQuotes(quote_page)
 
 # writes all quotes in list to a text file
 print("Writing quotes to file")
-file = open((topic+"_quoteList.txt"), "w")
+file = open((topic + "_quoteList.txt"), "w")
 for item in quotes:
     file.write("%s\n" % item)
 file.close()
